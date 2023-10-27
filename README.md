92b6e1ad0a64162363b3d0760a5da66f785c70d1
juanferron <meryjuan2019@gmail.com>
2023-10-27 12:13:39 +0200

    Ready for movement

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Este proyecto tiene como objetivo crear un juego de damas en el que el tablero s
 
 ## Descripción del Proyecto
 
-En este proyecto, estamos desarrollando un juego de damas que presenta un tablero de 8x8 cuadros. La característica principal de este proyecto es que el tablero se crea de forma dinámica en el navegador utilizando JavaScript. Los cuadros del tablero se generan automáticamente con IDs y estilos específicos.
+En este proyecto, estamos desarrollando un juego de damas que presenta un tablero de 8x8 cuadros. La característica principal de este proyecto es que el tablero y las piezas se crea de forma dinámica en el navegador utilizando JavaScript. Las casillas del tablero se generan automáticamente con IDs y estilos específicos.
 
 ## Funcionalidades Principales
 
-- Creación dinámica de un tablero de 10x10
+- Creación dinámica de un tablero de 8x8
 - Asignación automática de IDs a las casillas en formato "letra + número".
 - Estilo de casillas alternando entre blanco y negro.
 - Interacción con las casillas, mostrando el ID en la consola al hacer clic en ellas.
--- js/tablero.js
+++ js/tablero.js
@@ -4,6 +4,11 @@ const letras = ["H", "G", "F", "E", "D", "C", "B", "A"];
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8];
 const IDs_inicio = ["A1","C1","B2","A3","C3","B4","A5","C5","B6","A7","C7","B8","F1","H1","G2","F3","H3","G4","F5","H5","G6","F7","H7","G8"];
 const casillas = document.querySelectorAll(".casilla");
+const idpieza=""; //todas las piezas tienen un idpieza que es su id. total 24 idpiezas posibles
+const idtablero=""; //todas las casillas tienen un idtablero que es su id.total 64 idtableros posibles
+let estado=""; //estado de la casilla: vacia, con_pieza_blanca, con_pieza_negra, reina_blanca, reina_negra
+
+
 
 // Función para crear el grid del tablero de 8x8
 const crearGrid = () => {
@@ -13,7 +18,7 @@ const crearGrid = () => {
   gridcontainer.style.backgroundColor = "grey";
   gridcontainer.style.width = "400px";
   gridcontainer.style.maxHeight = "400px";
-  gridcontainer.style.margin = "2rem auto";
+  gridcontainer.style.margin = "0 auto";
   gridcontainer.style.padding = "auto";
   gridcontainer.style.border = "10px solid maroon";
 };
@@ -24,27 +29,23 @@ const crearTablero = () => {
   for (let i = 0; i < numeros.length; i++) {
     for (let j = 0; j < letras.length; j++) {
       const casilla = document.createElement("div");
-      const id = `${letras[i]}${numeros[j]}`;
-      casilla.setAttribute("idtablero", id);
-      // casilla.innerHTML = id;
+      const idtablero = `${letras[i]}${numeros[j]}`;
+      casilla.setAttribute("idtablero", idtablero);
+      casilla.setAttribute("estado", "vacia");
+      // casilla.innerHTML = id; ya no necesito mostrar el id de la casilla
       casilla.classList.add("casilla");
       gridcontainer.appendChild(casilla);
-      estiloCasilla(casilla);
-      casilla.addEventListener("click", () => {
-        console.log(`ID de la casilla: ${id}`);
-      });
+      estiloCasilla(casilla);    
     }
   }
 };
-
 // Función para dar estilos a las casillas
 const estiloCasilla = (casilla) => {
   casilla.style.width = "50px";
   casilla.style.height = "50px";
-  casilla.style.fontSize = "1.5rem";
+  casilla.style.fontSize = "1rem";
   casilla.style.textAlign = "center";
   casilla.style.lineHeight = "50px"; //
-  casilla.style.color = "blue";
 };
 // Función para dar estilos a las casillas, uno blanco y uno negro
 function pintarTablero() {
@@ -52,34 +53,63 @@ function pintarTablero() {
   casillas.forEach((casilla, index) => {
     const fila = Math.floor(index / 8);
     if ((fila % 2 === 0 && index % 2 === 0) || (fila % 2 === 1 && index % 2 === 1)) {
-      casilla.style.backgroundColor = "white";
-    } else {
+      casilla.style.backgroundColor = "white";  
+    } else {    
       casilla.style.backgroundColor = "black";
     }
   });
 }
-function colocarPieza(idtablero) {
+// Función para colocar las piezas en las casillas iniciales
+function colocarPiezasIniciales() {
+  IDs_inicio.forEach((id) => {
+    colocarPieza(id);
+  });
+}
+
+function colocarPieza(idtablero) { //
   // Encuentra la casilla con el atributo "idtablero" igual al valor de idtablero
   const casilla = document.querySelector(`[idtablero="${idtablero}"]`);
-
   if (casilla) {
     // Crea y agrega la ficha a la casilla con el idtablero especificado
     const pieza = document.createElement("img");
-    pieza.setAttribute("id", idtablero);
+    
     //si el idtablero tiene de letra A B o C
     if (["A","B","C"].includes(idtablero.charAt(0))){
-    pieza.classList.add("piezablanca"); 
+    pieza.classList.add("piezablanca");
+    //cambio estado de casilla
+    casilla.setAttribute("estado", "con_pieza_blanca");
     } else {
     pieza.classList.add("piezanegra"); 
+    //cambio estado de casilla
+    casilla.setAttribute("estado", "con_pieza_negra");
     }
     pieza.src = "./assets/imagenes/pieza.png"; // La imagen de la pieza
-    casilla.appendChild(pieza);
+    casilla.appendChild(pieza); // Agrega la pieza a la casilla 
   }
 }
-// Función para colocar las piezas en las casillas iniciales
-function colocarPiezasIniciales() {
-  IDs_inicio.forEach(id => {
-    colocarPieza(id);
+// Función para verificar el estado de las piezas
+function estadoPieza(pieza) {
+  casillas.forEach(casilla => {
+    //si la casilla tiene una pieza
+    if (casilla.hasChildNodes()) {
+      //si la pieza es blanca
+      if (pieza.classList.contains("piezablanca")) {
+        casilla.setAttribute("estado", "con_pieza_blanca");
+      } else {
+        casilla.setAttribute("estado", "con_pieza_negra");
+      }
+    } else {
+      casilla.setAttribute("estado", "vacia");
+    }
+});
+}
+
+// Función para verificar el estado de las casillas y muestre cual tiene una pieza y cual no
+function infoTablero() {
+  const casillas = document.querySelectorAll(".casilla");
+  casillas.forEach(casilla => {
+    const estado = casilla.getAttribute("estado");
+    console.log(`La casilla ${casilla.getAttribute("idtablero")} tiene el estado: ${estado}`);
   });
 }
 
@@ -89,12 +119,24 @@ function colocarPiezasIniciales() {
 
 
 
-
-
-
-
-window.onload = () => {
+//----------------------------------------------------------------//
+document.addEventListener("DOMContentLoaded", () => {
   crearTablero();
   pintarTablero();
   colocarPiezasIniciales();
-};
+  
+  // // Para verificar el estado de las casillas y las piezas:
+  // infoTablero(); 
+  // const piezas = document.querySelectorAll(".ficha");
+  // piezas.forEach(pieza => {
+  //   estadoPieza(pieza); // Muestra el estado de cada pieza
+  //   // Muestra el estado de las casillas
+  // });
+});
+
+// si dentro de mi gridcontainer hago click en una casilla me muestra su id
+gridcontainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("casilla")) {
+    console.log(e.target.getAttribute("idtablero") + " " + e.target.getAttribute("estado"));
+  }
+});
